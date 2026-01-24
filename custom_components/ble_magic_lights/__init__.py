26dea24716236d8d2e5cf6475d626bbc5347766c
Ajay Singh <admin@ajaysingh.com.np>
2026-01-24 21:11:53 +0545

    setup fixed

--- custom_components/ble_magic_lights/__init__.py
+++ custom_components/ble_magic_lights/__init__.py
@@ -1,11 +1,13 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
+PLATFORMS = ["light"]
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    await hass.config_entries.async_forward_entry_setup(entry, "light")
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    return await hass.config_entries.async_forward_entry_unload(entry, "light")
+    return await hass.config_entries.async_forward_entry_unload(entry, PLATFORMS)
